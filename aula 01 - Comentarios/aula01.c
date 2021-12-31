d0531eb51befa3b0e856430c9a3bb97ccc1890f6
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2021-12-31 14:16:45 -0300

    Criacao aula01.c

---
+++ aula 01 - Comentarios/aula01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    //Este é um comentario em linha
+    printf("Aprendendo Comentarios na Linguagem C\n");
+
+    /*
+    Este é um
+    grande bloco 
+    de comentarios
+    */
+    return 0;
+}
