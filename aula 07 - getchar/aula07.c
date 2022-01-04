b51ef7fb63cb92390dc97b97ce444a899ffabcab
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-04 14:14:38 -0300

    Criacao aula07.c

---
+++ aula 07 - getchar/aula07.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    char letra;
+    printf("Digite uma letra: ");
+    letra = getchar();
+
+    printf("Caracter lido: %c\n", letra);
+    return 0;
+}
