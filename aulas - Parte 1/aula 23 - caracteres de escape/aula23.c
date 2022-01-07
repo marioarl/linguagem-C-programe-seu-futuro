a5286ec1227656bcfa1421ac76712fe958400634
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-07 10:27:23 -0300

    Criacao aula23.c

---
+++ aulas - Parte 1/aula 23 - caracteres de escape/aula23.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+            Aula 23
+    Caracteres de escape
+    Quebra de linha                 -> \n
+    Tabulacao                       -> \t
+    Aspa dupla                      -> \"
+    Aspa simples                    -> \'
+    Contrabarra                     -> \\
+    Alarme sonoro do sistema        ->\a
+    Apaga o caracter a esquerda     ->\b
+    Volta para o inicio da linha    ->\r
+    Caracter nulo                   ->\0
+    Tabulacao vertical              -> \v
+
+*/
+
+
+int main(int argc, char const *argv[])
+{
+    printf("\n\n\tIsto é uma mensagem \"a\" \\ ser impressa na tela.\n\n");
+    return 0;
+}
