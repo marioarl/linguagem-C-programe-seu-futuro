1b870398a436f1bb2372f71244cb20d280b1b5d0
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-07 17:56:13 -0300

    Criacao aula25.c

---
+++ aulas - Parte 1/aula 25 - casting ou conversao/aula25.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int a = 10, b = 20;
+
+    printf("Soma: %d\n", a + b);
+    printf("Subtracao: %d\n", a - b);
+    printf("Multiplicacao %d\n", a + b);
+    printf("Divisao: %f\n", (float)a / b); //Conversao tipo de variavel a = 10.0
+    return 0;
+}
