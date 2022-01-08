3e489d35d50919375dce53f079290eb0a46389ba
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-08 09:39:01 -0300

    Criacao aula26.c

---
+++ aulas - Parte 1/aula 26 - usando o resto da divisao/aula26.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    //Resto da diviso %
+    int a = 10 , b = 20;
+    printf("Resto da divisao inteira: %d\n", a % 2); //saber se o numero é par ou impar resto 0 = par , resto 1 = impar
+
+
+    return 0;
+}
