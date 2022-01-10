7d00759a0bd14145aac2c949cd6a02753239561d
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-10 12:27:00 -0300

    Criacao aula40.c

---
+++ aulas - Parte 2/aula 40 - Estrutura de decisao p2/aula40.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int a;
+
+    printf("Digite um valor qualquer: ");
+    scanf("%d", &a);
+
+    if(a < 0)
+        printf("\n\tValor nagativo!\n");
+    else
+        printf("\n\tValor positivo ou igual a zero!\n");
+    
+    printf("\nContinuação do programa...\n");
+
+
+    return 0;
+}
