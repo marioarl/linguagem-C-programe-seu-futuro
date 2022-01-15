3cadf39ba32de794f249adb38a48235a6eb2579d
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-15 10:06:55 -0300

    Criacao aula66.c

---
+++ aulas - Parte 2/aula 66 - FOR incremento diferente de 1/aula66.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int i;
+
+    //Imprimindo numeros impares de 1 ate 100
+    for (i = 1; i <=100; i+=2){ //O mesmo que i = i + 2
+        printf("%d ", i);
+    }
+    
+    //Imprimindo numeros multiplos de 5 até 100
+    for (i = 1; i <=100; i+=2){ //O mesmo que i = i + 2
+        printf("%d ", i);
+    }
+
+    return 0;
+}
