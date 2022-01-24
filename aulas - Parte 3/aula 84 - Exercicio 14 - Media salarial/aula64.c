0f01d110c6e6d80d4e06caa8075a6b5b8871a3de
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-24 17:55:23 -0300

    Criacao aula64.c

---
+++ aulas - Parte 3/aula 84 - Exercicio 14 - Media salarial/aula64.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+Faça um programa que calcule a media de salarios de uma emrpresa, pedindo ao usuario a quantidade de funcionarios e o salario de cada funcionario.
+Ao final, o programa deve imprimir a media dos salarios informados, o salario mais alto e o salario mais baixo.
+*/
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    int qtdFunc;
+    float salario;
+
+    printf("\nQuantidade de funcionarios: ");
+    scanf("%d", &qtdFunc);
+
+
+    printf("Qtd de func %d", qtdFunc);
+    
+
+
+
+
+    return 0;
+}
