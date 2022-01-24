2ee0ebc658c3d35b575f4d70db93e3642235b6c2
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-24 17:59:31 -0300

    Alteracao aula84.c

--- aulas - Parte 3/aula 84 - Exercicio 14 - Media salarial/aula64.c
+++ aulas - Parte 3/aula 84 - Exercicio 14 - Media salarial/aula64.c
@@ -7,22 +7,45 @@ Faça um programa que calcule a media de salarios de uma emrpresa, pedindo ao us
 Ao final, o programa deve imprimir a media dos salarios informados, o salario mais alto e o salario mais baixo.
 */
 
-
-
-
 int main(int argc, char const *argv[])
 {
     int qtdFunc;
-    float salario;
-
-    printf("\nQuantidade de funcionarios: ");
-    scanf("%d", &qtdFunc);
+    float salario, sumSalario = 0, altSalario, baixoSalario;
 
 
-    printf("Qtd de func %d", qtdFunc);
+    do
+    {
+        printf("\nQuantidade de funcionarios: ");
+        scanf("%d", &qtdFunc);
+    } while (qtdFunc < 0);
+    
     
 
+    for(int i = 1; i <= qtdFunc; i++){
+        printf("\nDigite o salario do %do. funcionario: ", i);
+        scanf("%f", &salario);
+        if (i == 1){
+            altSalario = salario;    
+        } 
+        if(salario > altSalario){
+                altSalario = salario;
+            }
+        if (i == 1){
+            baixoSalario = salario;    
+        } 
+        if(salario < baixoSalario){
+                baixoSalario = salario;
+            }
 
+        sumSalario += salario;
+    }
+
+
+    printf("\n\tQuantidade de funcionarios:...... %d", qtdFunc);
+    printf("\n\tMedia do salario:................ R$ %.2f", sumSalario/qtdFunc);
+    printf("\n\tSalario mais alto:............... R$ %.2f", altSalario);
+    printf("\n\tSalario mais baixo:.............. R$ %.2f", baixoSalario);
+    
 
 
     return 0;
