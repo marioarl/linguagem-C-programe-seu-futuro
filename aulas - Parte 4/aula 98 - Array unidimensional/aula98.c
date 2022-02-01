6818ca140b217403f0d1f73b3dfe748f5c13b2b0
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-01 09:34:54 -0300

    Criacao aula98.c

---
+++ aulas - Parte 4/aula 98 - Array unidimensional/aula98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+- Para ler a idade do usuario é necessario uma variavel do tipo inteiro: int idade;
+- Para ler duas notas de um estudante sao necessarias duas variaveis do tipo float: float n1,n2;
+- E se for necessario ler a idade de 50 pessoas? Serao criadas 50 variaveis?
+
+- POr isso os ARRAYS(Vetor) sao imortantes, armazenando um conjunto de variaveis em um ARRAY unidimensional
+
+Um vetor é um conjunto de elementos do mesmo tipo armazenados de forma sequencial na memoria.
+
+Se voce criar um vetor com numeros inteiros, este vetor só aceitará numeros inteiros.
+Sao armazenados de forma sequencial dentro do Vetor.
+Um indice é usado para acessar cada elemento do vetor(sempre comecando em 0)
+*/
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
