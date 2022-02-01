b2f6dca9c94affc8cf6dc7ea26d2326fe3639769
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-01 09:45:35 -0300

    Criacao aula99.c

---
+++ aulas - Parte 4/aula 99 - Como criar um vetor/aula99.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+    int num1[10]; //Criando um vetor que irá armazenar 10 numeros inteiros.
+    //Outra forma de criar um Vetor: int num2[] = {1,2,5,9,15,43}; Ja fornecendo os numeros
+
+    int num5[5] = {0}; //Atribuindo um vetor com 0, pois poderá ter lixo de memoria
+
+    char letras[100]; 
+    char vogais[5] = {'a','e','i','o','u'};
+
+    float notas[3] = {7.5,8.3,9.5};
+    
+    return 0;
+}
