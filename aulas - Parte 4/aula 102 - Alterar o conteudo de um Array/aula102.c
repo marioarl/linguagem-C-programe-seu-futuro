348a8919ae5feb5c671270dd84f70cf12bd40d18
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-02 11:37:12 -0300

    Criacao aula102.c

---
+++ aulas - Parte 4/aula 102 - Alterar o conteudo de um Array/aula102.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    int num2[10];
+
+    for(i = 0; i < 10; i++){
+        printf("Digite o elemento da posicao %d: ", i);
+        scanf("%d", &num2[i]);
+    }
+
+    //Multiplicar cada elemento do vetor por 3 e salvar na mesma osicao
+    for(i = 0; i <10; i++){
+        num2[i] = num2[i] * 3;
+    }
+
+
+
+
+    printf("\n\n");
+
+    for(i = 0; i <10; i++){
+        printf("%d ", num2[i]);
+    }
+    printf("\n\n");
+
+
+
+    return 0;
+}
