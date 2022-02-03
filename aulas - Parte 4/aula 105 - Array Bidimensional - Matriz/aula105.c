707a8f684d4e1d0f252adbfe212775b50bb61e5a
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-03 09:42:46 -0300

    Criacao aula105.c

---
+++ aulas - Parte 4/aula 105 - Array Bidimensional - Matriz/aula105.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Matriz ==> É um conjunto de elementos do mesmo tipo (2 dimensoes , linhas e colunas)
+    
+             COLUNAS
+           0    1    2   3
+LINHAS   ___________________
+    0   | 21 | 73 | 91 | 31| 
+        |------------------| 
+    1   |14  | 66 | 21 | 52|
+        |------------------|
+    2   |15  | 81 | 11 | 37|
+        |------------------|
+
+mat[1][0] = 14  (linha e coluna)
+*/
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
