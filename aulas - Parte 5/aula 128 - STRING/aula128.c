16d8b9fdc99199607eaf962f3b5183235912c5e2
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-02 12:05:23 -0300

    Criacao aula128.c

---
+++ aulas - Parte 5/aula 128 - STRING/aula128.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+STRING - Sequencia de caracteres, um vetor
+
+
+
+*/
+
+
+int main(int argc, char const *argv[])
+{
+    char palavras[] = {"Ola Mundo!!"}; //String
+
+    printf("%c", palavras[4]); //Se quisermos imprimir a 4 letra desta string (M)
+    printf("%s\n\n", palavras); // Irá imprimir a String inteira
+    
+    return 0;
+}
