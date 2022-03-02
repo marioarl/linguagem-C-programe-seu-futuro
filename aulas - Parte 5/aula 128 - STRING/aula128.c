3049fb9351e2ba4e041a63315675985454a8af53
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-02 12:17:06 -0300

    Alteracao aula128.c

--- aulas - Parte 5/aula 128 - STRING/aula128.c
+++ aulas - Parte 5/aula 128 - STRING/aula128.c
@@ -11,7 +11,7 @@ STRING - Sequencia de caracteres, um vetor
 
 int main(int argc, char const *argv[])
 {
-    char palavras[] = {"Ola Mundo!!"}; //String
+    char palavras[] = {"Ola Mundo!!"}; //String, pode informar o tamanho da string
 
     printf("%c", palavras[4]); //Se quisermos imprimir a 4 letra desta string (M)
     printf("%s\n\n", palavras); // Irá imprimir a String inteira
